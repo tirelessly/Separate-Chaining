e2d86eef5747c879374c0f085c6adef6c90d75d7
Ruslan Jelbuldin <a01407036@unet.univie.ac.at>
2019-03-15 23:52:33 +0100

    first commit

---
+++ Separate-Chaining/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Separate-Chaining
+//
+//  Created by Ruslan Jelbuldin on 15.03.19.
+//  Copyright © 2019 Ruslan Jelbuldin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
